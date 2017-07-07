110e3131b0b80e2e009c8c79656fa4c2e38e7bbc
Dhanashree Kashid, Ekta Khanna, Jemish Patel and Jesse Zhang <pair+dkashid+ekhanna+jpatel+jzhang@pivotal.io>
2017-07-07 15:45:43 -0700

    squash it

--- concourse/check_existing_tag.bash
+++ concourse/check_existing_tag.bash
@@ -6,8 +6,8 @@ set -u -e -x
 
 main() {
 	TAG_TO_BE_PUBLISHED=$(head -1 orca_github_release_stage/tag.txt)
-	EXISTING_GIT_TAG=$(env GIT_DIR=orca_src/.git git describe --tags)
-	if [[ $TAG_TO_BE_PUBLISHED -eq $EXISTING_GIT_TAG ]]; then
+	EXISTING_GIT_TAG=$(env GIT_DIR=orca_src/.git git describe --abbrev=0 --tags)
+	if [[ "$TAG_TO_BE_PUBLISHED" -eq "$EXISTING_GIT_TAG" ]]; then
 		echo "Tag $TAG_TO_BE_PUBLISHED already present on ORCA repository"
 		echo "Please BUMP the ORCA version"
 		exit 1
